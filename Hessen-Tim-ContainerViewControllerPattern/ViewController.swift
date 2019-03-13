61d5de6859674dd8aaf1a5df4fddba473e066ea5
Michael Rojkov <michael.rojkov@awesome-technologies.de>
2019-03-13 15:23:22 +0100

    Initial Commit

---
+++ Hessen-Tim-ContainerViewControllerPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hessen-Tim-ContainerViewControllerPattern
+//
+//  Created by Michael Rojkov on 13.03.19.
+//  Copyright © 2019 Michael Rojkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
