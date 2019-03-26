b159b38c4df89419b68b68eeae7fcb20f651ce07
Michael Rojkov <michael.rojkov@awesome-technologies.de>
2019-03-26 15:49:43 +0100

    First commit of the work in progress Hessen-Tim app

---
+++ Hessen-Tim-ContainerViewControllerPattern/BaseViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Hessen-Tim-ContainerViewControllerPattern
+//
+//  Created by Michael Rojkov on 13.03.19.
+//  Copyright © 2019 Michael Rojkov. All rights reserved.
+//
+
+import UIKit
+
+/*
+ Helping Tutorials:
+ 
+ https://naveenr.net/beginning-container-views-in-ios/
+ 
+ */
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    var galleryVC:GalleryViewController?
+    var cameraVC:CameraPictureViewController?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let vc = segue.destination as? GalleryViewController {
+            galleryVC = vc
+            
+        }
+        
+        if let vc = segue.destination as? CameraPictureViewController {
+            cameraVC = vc
+            cameraVC?.delegate = self
+        }
+    }
+    
+    
+    //Action
+    @IBAction func takePicture(_ sender: Any) {
+        cameraVC?.makePhoto()
+    }
+    
+}
+
+
+
+
+
+extension BaseViewController: GalleryDelegate {
+    func addGalleryImage(imageName: String) {
+        //print("added an Image")
+        galleryVC?.insertItemTest(imageName: imageName)
+    }
+}
